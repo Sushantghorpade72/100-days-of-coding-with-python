b7cd659c7baab067882be0009536351bd9fa4190
Sushant Ghorpade <63212782+Sushantghorpade72@users.noreply.github.com>
2021-10-04 16:50:57 -0400

    Add files via upload

---
+++ Day 1/Day1_Brand_Name_Generator.py
@@ -0,0 +1,16 @@
+'''
+Project Name: Brand Name Generator
+Author: Sushant
+
+'''
+#1.Create a greeting for your program
+print("Welcome to Brand Name Generator")
+print("-"*40)
+#2.Ask user for the city you grew in
+city = str(input("Which city you grew in? "))
+print("-"*40)
+#3. Ask the user for the name of a pet
+pet = str(input("Name of your pet? "))
+print("-"*40)
+#4. Combine Name of city and pet name to show brand name
+print(city +" "+pet+"'s")
