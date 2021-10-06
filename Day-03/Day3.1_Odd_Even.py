88e5a998ca2ac12362debf1124e3bcdf2eb84eab
Sushant Ghorpade <63212782+Sushantghorpade72@users.noreply.github.com>
2021-10-06 18:22:12 -0400

    Add files via upload
    Check if input number is odd or even

---
+++ Day-03/Day3.1_Odd_Even.py
@@ -0,0 +1,12 @@
+'''
+Project Name: Odd or even
+Author: Sushant
+
+Create a programme to check if a number is ood or even
+'''
+num = int(input("Enter your number:"))
+
+if num%2 == 0:
+    print("Even")
+else:
+    print("Odd")
